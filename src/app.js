3e889ffb8ebae8db53ed8febfd3105e0c1056c64
Przemyslaw Koziel <mos.koziel@gmail.com>
2020-01-07 20:01:18 +0100

    Forecast

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './styles/styles.css';
 
 function App() {
-  return <div>Hello</div>;
+  const header = {
+    display: 'flex',
+    alignContent: 'center',
+    justifyContent: 'center',
+    marginTop: '300px',
+    fontSize: '24px'
+  };
+  return <div style={header}>Weather tomorrow is the same as today</div>;
 }
 ReactDOM.render(<App />, document.getElementById('root'));
